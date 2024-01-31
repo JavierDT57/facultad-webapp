8e6a8ec32757a16b82c32edf073d808c1aea8e6f
JavierDT57 <javierdolorestolentino@gmail.com>
2024-01-31 16:08:38 -0600

    Previos completados del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
+import { RegistroUsuarioScreenComponent } from './screens/registro-usuario-screen/registro-usuario-screen.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Angular Material Elements
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginScreenComponent,
+    RegistroUsuarioScreenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+//URL del entorno virutual del proyecto
+export const environment = {
+  production: false,
+  url_api: "http://127.0.0.1:8000"
+  // url_api: "https://api-dot-kocina-arte.wl.r.appspot.com"
+};
